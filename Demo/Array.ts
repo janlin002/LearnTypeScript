7561309ca0bbd8c85cc47477c264a210e77ba138
janlin <123davidbill@gmail.com>
2021-07-22 09:52:26 +0800

    add array

---
+++ Demo/Array.ts
@@ -0,0 +1,11 @@
+let arr1:number[] = [1,2,3]
+let arr2:string[] = ['1', '2', '3']
+let arr3:Array<number> = [1,2,3]
+let arr4:Array<string> = ['1', '2', '3']
+let arr5:number[] = new Array(1,2,3)
+
+console.log(arr1) //[1,2,3]
+console.log(arr2) //['1', '2', '3']
+console.log(arr3) //[1,2,3]
+console.log(arr4) //['1', '2', '3']
+console.log(arr5) //[1,2,3]
