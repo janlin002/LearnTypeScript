693ddea6aa2818a2e3800fa37a9fdf65c58b3b6d
janlin <123davidbill@gmail.com>
2021-08-07 16:16:46 +0800

    generics

--- Demo/Generics.ts
+++ Demo/Generics.ts
@@ -2,5 +2,26 @@ function join(a:string|number, b:string|number){
     return `${a} ${b}`
 }
 
-const result = join('hello', 'world')
-console.log(result)
+const result = join('hello', 'world');
+const result2 = join(12, 14)
+const result3 = join('hello', 2)
+// console.log(result)
+// console.log(result2)
+// console.log(result3)
+
+function invite<jan>(a:jan, b:jan){
+    return a;
+}
+
+const hello = invite<number>(1,2);
+console.log(hello)
+
+
+// 泛型array類型
+function arrayItem<ANY>(a:ANY[]){
+    return a
+}
+
+const arrResult = arrayItem<number>([1]);
+
+console.log(arrResult, 'arrResult')
